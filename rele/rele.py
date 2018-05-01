98e0acef11d3b5c48327a7d5d944d44886f124ea
Daniel Kyzlink <daniel.kyzlink@gmail.com>
2018-05-01 19:48:16 +0000

    naroubovani funkci z zarovky.py

--- rele/rele.py
+++ rele/rele.py
@@ -1,29 +1,47 @@
 import requests
 import json
 import time
+import zarovky
+import atexit
 
 rele0 = None
 rele1 = None
 
-# stažení stránky
+zarovky.myinit()
+
+atexit.register(zarovky.cleanup)
+
+# stazeni stranky
 while (True):
-    try:            
+    try:
         myjson = requests.get('http://py.prestcs.cz/rele.json')
-        # ověření, že dotaz proběhl v pořádku
+        # overeni, ze dotaz probehl v poradku
         myjson.raise_for_status()
         data = json.loads(myjson.text)
     except Exception:
-        print('soubor není v pořádku')
+        print('soubor neni v poradku')
     else:        
 
         if rele0 != data['rele0']:
             rele0 = (data['rele0'])
             print("rele0:")
             print(rele0)
+            if rele0:
+                print("rele0 zapinam")
+                zarovky.zapni4()
+            else:
+                print("rele0 vypinam")
+                zarovky.vypni4()
 
         if rele1 != data['rele1']:
             rele1 = (data['rele1'])
             print("rele1:")
             print(rele1)
+            if rele1:
+                print("rele1 zapinam")
+                zarovky.zapni17()
+            else:
+                print("rele1 vypinam")
+                zarovky.vypni17()
 
     time.sleep(2)
